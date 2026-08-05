09ec6d789f806e0580943fad9a803b850dfa7e37
abhi19-code <abhiyadav812767@gmail.com>
2026-08-05 23:23:36 +0530

    Add CRUD API

--- server.js
+++ server.js
@@ -3,8 +3,17 @@ const express = require("express");
 const app = express();
 const PORT = 5000;
 
-// Middleware to read JSON data
+// Read JSON data
 app.use(express.json());
+
+// Temporary database
+let users = [
+  {
+    id: 1,
+    name: "Abhi",
+    course: "Node.js"
+  }
+];
 
 // Home route
 app.get("/", (req, res) => {
@@ -13,7 +22,67 @@ app.get("/", (req, res) => {
   });
 });
 
-// Start server
+// Get all users
+app.get("/users", (req, res) => {
+  res.json(users);
+});
+
+// Add new user
+app.post("/users", (req, res) => {
+  const newUser = {
+    id: users.length + 1,
+    name: req.body.name,
+    course: req.body.course
+  };
+
+  users.push(newUser);
+
+  res.status(201).json({
+    message: "User added",
+    user: newUser
+  });
+});
+
+// Update user
+app.put("/users/:id", (req, res) => {
+  const id = Number(req.params.id);
+
+  const user = users.find((item) => item.id === id);
+
+  if (!user) {
+    return res.status(404).json({
+      message: "User not found"
+    });
+  }
+
+  user.name = req.body.name || user.name;
+  user.course = req.body.course || user.course;
+
+  res.json({
+    message: "User updated",
+    user
+  });
+});
+
+// Delete user
+app.delete("/users/:id", (req, res) => {
+  const id = Number(req.params.id);
+
+  const user = users.find((item) => item.id === id);
+
+  if (!user) {
+    return res.status(404).json({
+      message: "User not found"
+    });
+  }
+
+  users = users.filter((item) => item.id !== id);
+
+  res.json({
+    message: "User deleted"
+  });
+});
+
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
